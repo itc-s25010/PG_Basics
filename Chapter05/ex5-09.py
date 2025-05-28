7833ec20124fd1e8ecb839c8edf17efe7184085d
vagrant <s25010@std.it-college.ac.jp>
2025-05-28 04:18:24 +0000

    P70-P86 第５章演習 終了

---
+++ Chapter05/ex5-09.py
@@ -0,0 +1,3 @@
+colors1 = ["blue", "green", "yellow"]
+colors2 = ["orange", "pink", "black"]
+print(colos1 + colors2)
---
+++ Chapter05/ex5-25.py
@@ -0,0 +1,5 @@
+eights = ["Edgar Allan Poe", "Charles Dickens"]
+nines = ["Hemingway", "fitzgeald", "Orwell"]
+
+authors = (eights, nines)
+print(authors)
